2acad99cb1909408f8cdd652c62c1a544d91dce8
Alex Engelhart <asengelhart@yahoo.com>
2019-12-10 19:17:54 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,19 @@ def oxford_comma(array)
   elsif array.size == 2 
     return array.join(" and ")
   else 
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
+    result = ""
+    array.each_with_index do |word, index|
+      result += word
+      if index < array.length - 2
+        result += ", "
+      elsif index == array.length - 2 
+        result += ", and "
+      end 
+    end 
+  result
   end 
 end
+
+arr = ["foo", "bar", "baz"]
+puts oxford_comma(arr)
+puts arr

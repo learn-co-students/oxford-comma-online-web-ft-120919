08566f0e3044772ccd428a855def7d32976a1643
Benjamin Small <ben20262@aim.com>
2019-12-10 22:11:52 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
 def oxford_comma(array)
-
+  words = ""
+  if array.size > 2
+    array.each do |word|
+      if word == array[-1]
+        words = words + ", and " + word
+      elsif word == array[0]
+        words = word
+      else
+        words = words + ", " + word
+      end
+    end
+  elsif array.size == 2
+    words = array.join(" and ")
+  else
+    words = array[0]
+  end
+  words
 end

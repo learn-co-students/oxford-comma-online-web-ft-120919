84c006164b1f16d609fbdf6a4b759c22e12e4b27
Joanna E K <kalemajoanna@gmail.com>
2019-12-11 16:04:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join ""
+  elsif array.length == 2 
+  array.join (" and ")
+  elsif array.length >= 3
+array[-1] = "and #{array[-1]}"
+  array.join (", ")
 end
+end
+
+
+  
+ 
+# def oxford_comma(array)
+# array.join(",")
+# end
+
+# def oxford_comma(array)
+#   array.join(" and ")
+# end
+
+
+

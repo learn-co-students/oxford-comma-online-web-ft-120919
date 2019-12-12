2801e93eaf4d1ec9828bcaebcbec37994ba6da35
Ho Yin Keung <hykeung92@gmail.com>
2019-12-12 00:38:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    return array.join
+  when 2
+    return array.join(" and ")
+  else
+    array[-1].insert(0, "and ")
+  end
+  array.join(", ")
 end

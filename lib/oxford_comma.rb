0e98a10ad801d212f59ee1521064af3c4f9c5b3d
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-12 15:05:04 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+if array.length == 1 
+  return array[0] 
+ elsif array.length > 1 && array.length < 3
+        array.join(" and ")
+  else array.length > 2
+        array[-1].insert(0, "and ")
+        array.join(", ")
+    end
+end 
+ 
 
-end

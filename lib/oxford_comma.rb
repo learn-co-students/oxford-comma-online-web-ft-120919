c47b7a99616160d020bf93548b4cf01208f61cd0
Khadijah Jennings <kjennings855@gmail.com>
2019-12-14 06:35:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
-# def oxford_comma(array)
-#   fruits = ["kiwi"].join
-# end
-
-# oxford_comma << " durian "
-# end
-
-oxford_comma = ["kiwi"]
+def oxford_comma(array)
+  if array.size == 1
+  array.join
+  elsif array.size == 2
+  array.join(" and ")
+  else
+  last_str = array.pop
+  first_str = array.join (", ")
+  final_str = first_str +", and #{last_str}"
+  end
+end
